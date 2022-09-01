405eb2d52cb7bc79ebb4c7798daffdd91ad42158
Szabolcs Lukacs <lukasdevarga@gmail.com>
2022-09-01 13:19:23 +0200

    fix typo

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -30,7 +30,7 @@ apiRoute.use(upload.single('file'))
 
 apiRoute.post((req, res) => {
   console.log('IN THE POST:::', req.body)
-  res.status(200).json({ data: 'succes' })
+  res.status(200).json({ data: 'success' })
 })
 
 export default apiRoute
